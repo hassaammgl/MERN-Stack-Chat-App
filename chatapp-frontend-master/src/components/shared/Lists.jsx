4ac119397ea5bbad6edaea3c303ad0d5582a2516
hassaammgl <h455a4mmehtab@gmail.com>
2024-05-04 13:07:27 +0500

    adsf

--- chatapp-frontend-master/src/components/shared/Lists.jsx
+++ chatapp-frontend-master/src/components/shared/Lists.jsx
@@ -5,7 +5,7 @@ const Posts = [
     {
         title: 'Post 1',
         description: 'This is post 1 description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        image: "https://picsum.photos/200/300",
+        image: ["https://picsum.photos/200/300"],
         author: {
             name: 'John Doe',
             avatar: "https://www.w3schools.com/howto/img_avatar.png",
@@ -15,7 +15,7 @@ const Posts = [
     {
         title: 'Post 2',
         description: 'This is post 2 description. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Ut enim ad minim veniam, quis  ',
-        image: "https://imgs.search.brave.com/iDw7aVRV00p0wmn0B_Dq0mZhD1FKz5YT1gyO2GkLYl8/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTgy/NDY1MDk3L3Bob3Rv/L2h0bWwtY29kZS5q/cGc_cz02MTJ4NjEy/Jnc9MCZrPTIwJmM9/cUlIWldfc3JaTkZI/RzVjamVrbGkwLU9T/UzRZbjlWSWNtcFl5/V19jS3B3MD0",
+        image: ["https://imgs.search.brave.com/iDw7aVRV00p0wmn0B_Dq0mZhD1FKz5YT1gyO2GkLYl8/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTgy/NDY1MDk3L3Bob3Rv/L2h0bWwtY29kZS5q/cGc_cz02MTJ4NjEy/Jnc9MCZrPTIwJmM9/cUlIWldfc3JaTkZI/RzVjamVrbGkwLU9T/UzRZbjlWSWNtcFl5/V19jS3B3MD0"],
         author: {
             name: 'Jane Smith',
             avatar: "https://www.w3schools.com/howto/img_avatar2.png",
@@ -25,7 +25,7 @@ const Posts = [
     {
         title: 'Post 3',
         description: 'This is post 3 description. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-        image: "https://picsum.photos/200/302",
+        image: ["https://picsum.photos/200/302"],
         author: {
             name: 'Bob Johnson',
             avatar: "https://www.w3schools.com/howto/img_avatar3.png",
@@ -35,7 +35,7 @@ const Posts = [
     {
         title: 'Post 4',
         description: 'This is post 4 description. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        image: "https://picsum.photos/200/303",
+        image: ["https://picsum.photos/200/303"],
         author: {
             name: 'Sarah Williams',
             avatar: "https://www.w3schools.com/howto/img_avatar4.png",
@@ -45,7 +45,7 @@ const Posts = [
     {
         title: 'Post 5',
         description: 'This is post 5 description. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.',
-        image: "https://picsum.photos/200/304",
+        image: ["https://picsum.photos/200/304"],
         author: {
             name: 'Mark Davis',
             avatar: "https://www.w3schools.com/howto/img_avatar5.png",
@@ -55,7 +55,7 @@ const Posts = [
     {
         title: 'Post 6',
         description: 'This is post 6 description. Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
-        image: "https://picsum.photos/200/305",
+        image: ["https://picsum.photos/200/305"],
         author: {
             name: 'Pat Johnson',
             avatar: "https://www.w3schools.com/howto/img_avatar6.png",
@@ -65,7 +65,7 @@ const Posts = [
     {
         title: 'Post 7',
         description: 'This is post 7 description. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur.',
-        image: "https://picsum.photos/200/306",
+        image: ["https://picsum.photos/200/306"],
         author: {
             name: 'Anne Baker',
             avatar: "https://www.w3schools.com/howto/img_avatar7.png",
@@ -75,7 +75,7 @@ const Posts = [
     {
         title: 'Post 8',
         description: 'This is post 8 description. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt.',
-        image: "https://picsum.photos/200/307",
+        image: ["https://picsum.photos/200/307"],
         author: {
             name: 'Tim Wilson',
             avatar: "https://www.w3schools.com/howto/img_avatar8.png",
@@ -85,7 +85,7 @@ const Posts = [
     {
         title: 'Post 9',
         description: 'This is post 9 description. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-        image: "https://picsum.photos/200/308",
+        image: ["https://picsum.photos/200/308"],
         author: {
             name: 'Michelle Green',
             avatar: "https://www.w3schools.com/howto/img_avatar9.png",
@@ -95,7 +95,7 @@ const Posts = [
     {
         title: 'Post 10',
         description: 'This is post 10 description. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        image: "https://picsum.photos/200/309",
+        image: ["https://picsum.photos/200/309"],
         author: {
             name: 'Chris Jackson',
             avatar: "https://www.w3schools.com/howto/img_avatar10.png",
@@ -105,7 +105,7 @@ const Posts = [
     {
         title: 'Post 11',
         description: 'This is post 11 description. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.',
-        image: "https://picsum.photos/200/310",
+        image: ["https://picsum.photos/200/310"],
         author: {
             name: 'Emily Wilson',
             avatar: "https://www.w3schools.com/howto/img_avatar11.png",
@@ -115,7 +115,7 @@ const Posts = [
     {
         title: 'Post 12',
         description: 'This is post 12 description. Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
-        image: "https://picsum.photos/200/311",
+        image: ["https://picsum.photos/200/311"],
         author: {
             name: 'Olivia Davis',
             avatar: "https://www.w3schools.com/howto/img_avatar12.png",
@@ -125,7 +125,7 @@ const Posts = [
     {
         title: 'Post 13',
         description: 'This is post 13 description. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur.',
-        image: "https://picsum.photos/200/312",
+        image: ["https://picsum.photos/200/312"],
         author: {
             name: 'Ava Jackson',
             avatar: "https://www.w3schools.com/howto/img_avatar13.png",
@@ -135,7 +135,7 @@ const Posts = [
     {
         title: 'Post 14',
         description: 'This is post 14 description. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt.',
-        image: "https://picsum.photos/200/313",
+        image: ["https://picsum.photos/200/313"],
         author: {
             name: 'Noah Green',
             avatar: "https://www.w3schools.com/howto/img_avatar14.png",
@@ -145,7 +145,7 @@ const Posts = [
     {
         title: 'Post 15',
         description: 'This is post 15 description. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        image: "https://picsum.photos/200/314",
+        image: ["https://picsum.photos/200/314"],
         author: {
             name: 'Logan Wilson',
             avatar: "https://www.w3schools.com/howto/img_avatar15.png",
