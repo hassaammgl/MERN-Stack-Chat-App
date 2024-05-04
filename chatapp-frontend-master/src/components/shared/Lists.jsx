5e5f68adc8b084061ccfc681703c870f2c821c6b
hassaammgl <h455a4mmehtab@gmail.com>
2024-05-04 13:26:36 +0500

    sadf

--- chatapp-frontend-master/src/components/shared/Lists.jsx
+++ chatapp-frontend-master/src/components/shared/Lists.jsx
@@ -191,7 +191,7 @@ const PostListItem = ({ post }) => {
                 <Box onClick={handlePostClick}>
                     <Stack style={{ padding: "2rem 1rem", boxShadow: "rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px" }} spacing={1} marginX={"6.5rem"} direction={"row"}>
                         <Box paddingRight={"3rem"}>
-                            <img src={post.image} alt={post.title} style={{ borderRadius: "5px" }} width="450px" height="250px" />
+                            <img src={post.image[0]} alt={post.title} style={{ borderRadius: "5px" }} width="450px" height="250px" />
                         </Box>
                         <Box>
                             <Typography variant="h6" fontWeight={"bold"} color={"gray"} marginBottom={"1.5rem"}>{post.title}</Typography>
